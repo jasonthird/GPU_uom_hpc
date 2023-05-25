3b5eed9c10ddc7a1f7c5ba85ce0e6bf3ce145a81
Iason Malkotsis <imalkotsis@gmail.com>
2023-05-25 16:40:06 +0300

    Update StringMatching.c

--- String match/StringMatching.c
+++ String match/StringMatching.c
@@ -1,102 +1,118 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include <hip/hip_runtime.h>
 
-__global__ void StringMatching(char* fileData,long fileSize, char* pattern, int patternSize, int* match, int matchSize,int *total_matches){
+#define MaxPatternSize 256
 
-    int i = blockIdx.x * blockDim.x + threadIdx.x;  
+__global__ void StringMatching(char* fileData, long fileSize, char* pattern, int patternSize, int* match, int matchSize, int* total_matches) {
+    __shared__ char sharedPattern[MaxPatternSize];
+
+    // Load pattern into shared memory
+	int tid = threadIdx.x;
+    if (tid < patternSize) {
+        sharedPattern[tid] = pattern[tid];
+    }
+    __syncthreads();
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (i >= matchSize)
-		return;
-	
-	int j;
-	for (j = 0; j < patternSize; j++){
-		if (fileData[i+j] != pattern[j])
-			break;
-	}
-	if (j == patternSize){
-		match[i] = 1;
-		atomicAdd(total_matches, 1);
-	}
+        return;
+
+    int j;
+    int localMatch = 1; // Local variable to track matching status
+    for (j = 0; j < patternSize; j++) {
+        if (fileData[i + j] != sharedPattern[j]) {
+            localMatch = 0;
+            break;
+        }
+    }
+        // Use atomicAdd with total_matches
+    if (localMatch) {
+		match[i]=1;
+        atomicAdd(total_matches, 1);
+    }
 }
 
-int main (int argc, char *argv[]) {
-	
-	FILE *pFile;
-	long file_size;
-	char * buffer;
-	char * filename;
-	size_t result;
-	int *match;
-	int *total_matches;
-	char *pattern;
-	int patternSize;
 
-	if (argc != 3) {
-		printf ("Usage : %s <file_name> <string>\n", argv[0]);
-		return 1;
-	}
-	filename = argv[1];
-	pattern = argv[2];
-	patternSize = strlen(pattern);
-	pFile = fopen ( filename , "rb" );
-	if (pFile==NULL) {printf ("File error\n"); return 2;}
 
-	// obtain file size:
-	fseek (pFile , 0 , SEEK_END);
-	file_size = ftell (pFile);
-	rewind (pFile);
-	printf("file size is %ld\n", file_size);
-	
-	// allocate memory to contain the file:
-	buffer = (char*) malloc (sizeof(char)*file_size);
-	if (buffer == NULL) {printf ("Memory error\n"); return 3;}
+int main(int argc, char* argv[]) {
 
-	// copy the file into the buffer:
-	result = fread (buffer,1,file_size,pFile);
-	if (result != file_size) {printf ("Reading error\n"); return 4;}
-    
-    //setup gpu memory
-    char * fileDataGpu;
-    int * matchGpu;
-	int * total_matchesGpu;
-	char * patternGpu;
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " <file_name> <string>\n";
+        return 1;
+    }
 
-    hipMalloc((void**)&fileDataGpu, file_size * sizeof(char));
-	hipMalloc((void**)&matchGpu, file_size * sizeof(int));
-	hipMalloc((void**)&total_matchesGpu, sizeof(int));
-	hipMalloc((void**)&patternGpu, patternSize * sizeof(char));
+    std::string filename = argv[1];
+    std::string pattern = argv[2];
+    int patternSize = pattern.size();
 
-	hipMemset(matchGpu,0, file_size*sizeof(int));
-	hipMemset(total_matchesGpu,0, sizeof(int));
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cout << "File error\n";
+        return 2;
+    }
 
-    // //send data to gpu
-    hipMemcpy(fileDataGpu, buffer, file_size * sizeof(char), hipMemcpyHostToDevice);
-	// hipMemcpy(patternGpu, pattern, patternSize * sizeof(char), hipMemcpyHostToDevice);
-	
+    // obtain file size
+    file.seekg(0, std::ios::end);
+    long fileSize = file.tellg();
+    file.seekg(0, std::ios::beg);
+    std::cout << "File size is " << fileSize << std::endl;
 
-    // //setup execution parameters
-    // dim3 threadsPerBlock(512, 1, 1);
-    // dim3 blocks((file_size + threadsPerBlock.x - 1) / threadsPerBlock.x, 1, 1);
-    
-    // //launch kernel
-    // hipLaunchKernelGGL(StringMatching, blocks, threadsPerBlock, 0, 0, fileDataGpu, file_size, patternGpu, patternSize, matchGpu, file_size, total_matchesGpu);
+    // allocate memory to contain the file
+    std::vector<char> buffer(fileSize);
+    if (!file.read(buffer.data(), fileSize)) {
+        std::cout << "Reading error\n";
+        return 4;
+    }
 
-    
-    // //get results back
-    // hipMemcpy(match, matchGpu, file_size * sizeof(int), hipMemcpyDeviceToHost);
-	// hipMemcpy(total_matches, total_matchesGpu, sizeof(int), hipMemcpyDeviceToHost);
+    // setup GPU memory
+    char* fileDataGpu;
+    int* matchGpu;
+    int* total_matchesGpu;
+    char* patternGpu;
 
-	// printf("Total matches: %d\n", *total_matches);
-	
-	// //cleanup
-	// hipFree(fileDataGpu);
-	// hipFree(matchGpu);
-	// hipFree(total_matchesGpu);
-	// hipFree(patternGpu);
-	// fclose (pFile);
-	// free (buffer);
+    hipMalloc((void**)&fileDataGpu, fileSize * sizeof(char));
+    hipMalloc((void**)&matchGpu, fileSize * sizeof(int));
+    hipMalloc((void**)&total_matchesGpu, sizeof(int));
+    hipMalloc((void**)&patternGpu, patternSize * sizeof(char));
 
-	return 0;
+    hipMemset(matchGpu, 0, fileSize * sizeof(int));
+    hipMemset(total_matchesGpu, 0, sizeof(int));
+
+    // send data to GPU
+    hipMemcpy(fileDataGpu, buffer.data(), fileSize * sizeof(char), hipMemcpyHostToDevice);
+    hipMemcpy(patternGpu, pattern.c_str(), patternSize * sizeof(char), hipMemcpyHostToDevice);
+
+    // setup execution parameters
+    dim3 threadsPerBlock(512, 1, 1);
+    dim3 blocks((fileSize + threadsPerBlock.x - 1) / threadsPerBlock.x, 1, 1);
+
+    // launch kernel
+    hipLaunchKernelGGL(StringMatching, blocks, threadsPerBlock, 0, 0, fileDataGpu, fileSize, patternGpu, patternSize, matchGpu, fileSize, total_matchesGpu);
+
+    // get results back
+    std::vector<int> match(fileSize);
+    int total_matches;
+    hipMemcpy(match.data(), matchGpu, fileSize * sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(&total_matches, total_matchesGpu, sizeof(int), hipMemcpyDeviceToHost);
+
+	//print results
+    std::cout << "Total matches: " << total_matches << std::endl;
+	// for (int i=0; i<fileSize; i++) {
+	// 	if (match[i] == 1) {
+	// 		std::cout << "Match at index " << i << std::endl;
+	// 	}
+	// }
+
+    // cleanup
+    hipFree(fileDataGpu);
+    hipFree(matchGpu);
+    hipFree(total_matchesGpu);
+    hipFree(patternGpu);
+    file.close();
+
+    return 0;
 }
